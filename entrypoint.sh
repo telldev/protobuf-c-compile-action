5577307fd6074d51d441da3e8419be7afafe6ef7
Arpad Szent-Kirallyi <szent-kirallyi.arpad@tell.hu>
2019-10-21 12:59:20 +0200

    - add support for multi compile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 
-echo "Compiling $files to $output_dir";
+compile () {
+    if [ $# -lt 3 ]; then
+        return 0;
+    fi 
 
-protoc --c_out=$output_dir $files 
+    protoc --$1=$2 $3;
+    
+    if [ $? -ne 0 ]; then
+        echo "Compile error (protoc --$1=$2 $3)";
+        return -1;
+    fi;
+
+    return 0;
+}
+
+echo "Compiling $files";
+
+RESULT=0;
+
+compile "cpp_out" $cpp_out $files;
+compile "csharp_out" $csharp_out $files;
+compile "java_out" $java_out $files;
+compile "js_out" $js_out $files;
+compile "objc_out" $objc_out $files;
+compile "php_out" $php_out $files;
+compile "python_out" $python_out $files;
+compile "ruby_out" $ruby_out $files;
+compile "c_out" $c_out $files;
 
 echo "done.";
 
+exit $RESULT;
